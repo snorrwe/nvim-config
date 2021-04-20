23819b8eedc9085f3e65cf3a5f3c0f925b3d1221
Daniel Kiss <littlesnorrboy@gmail.com>
2021-04-20 21:35:26 +0200

    Add tokyonight theme, move more config to lua

--- init.vim
+++ init.vim
@@ -32,6 +32,8 @@ Plug 'vim-airline/vim-airline-themes'
 
 Plug 'junegunn/fzf', {'do': { -> fzf#install() }}
 Plug 'junegunn/fzf.vim'
+
+Plug 'folke/tokyonight.nvim'
 call plug#end()
 
 lua << EOF
@@ -44,28 +46,6 @@ let g:neovide_cursor_vfx_mode = "railgun"
 " fzf
 "
 nnoremap <C-P> :Files<CR>
-
-" autoformat
-"
-let g:formatter_path = ["c:/tools"]
-let g:autoformat_autoindent = 0
-let g:autoformat_retab = 0
-let g:formatters_js = ['prettier']
-let g:formatters_jsx = ['prettier']
-let g:formatters_ts = ['prettier']
-let g:formatters_javascript = ['prettier']
-let g:formatters_typescript = ['prettier']
-let g:formatters_javascriptreact = ['prettier']
-let g:formatters_typescriptreact = ['prettier']
-let g:formatters_html = ['prettier']
-let g:formatters_css = ['prettier']
-let g:formatters_json = ['prettier']
-let g:formatters_toml = ['prettier']
-let g:formatters_md = ['prettier']
-let g:formatters_yaml = ['prettier']
-let g:formatters_svelte = ['prettier']
-let g:formatters_sql = []
-let g:formatters_python = ['black']
 
 " NERDcommenter
 let g:NERDSpaceDelims = 1
@@ -108,39 +88,3 @@ if !exists("*ChangeCwdHere")
 	endfunction
 endif
 command! ChangeCwdHere call ChangeCwdHere()
-
-
-" lsp
-"
-set shortmess-=F
-set shortmess+=c
-
-set completeopt=menuone,noinsert,noselect
-
-" Code navigation shortcuts
-nnoremap <silent> <c-]> <cmd>lua vim.lsp.buf.definition()<CR>
-nnoremap <silent> K     <cmd>lua vim.lsp.buf.hover()<CR>
-nnoremap <silent> gD    <cmd>lua vim.lsp.buf.implementation()<CR>
-nnoremap <silent> <c-k> <cmd>lua vim.lsp.buf.signature_help()<CR>
-nnoremap <silent> 1gD   <cmd>lua vim.lsp.buf.type_definition()<CR>
-nnoremap <silent> gr    <cmd>lua vim.lsp.buf.references()<CR>
-nnoremap <silent> gR    <cmd>lua vim.lsp.buf.rename()<CR>
-nnoremap <silent> g0    <cmd>lua vim.lsp.buf.document_symbol()<CR>
-nnoremap <silent> gW    <cmd>lua vim.lsp.buf.workspace_symbol()<CR>
-nnoremap <silent> gd    <cmd>lua vim.lsp.buf.declaration()<CR>
-nnoremap <silent> ga    <cmd>lua vim.lsp.buf.code_action()<CR>
-
-" Set updatetime for CursorHold
-" 800ms of no cursor movement to trigger CursorHold
-set updatetime=800
-" Show diagnostic popup on cursor hold
-autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics()
-
-" Goto previous/next diagnostic warning/error
-nnoremap <silent> g[ <cmd>lua vim.lsp.diagnostic.goto_prev()<CR>
-nnoremap <silent> g] <cmd>lua vim.lsp.diagnostic.goto_next()<CR>
-
-" Enable type inlay hints
-autocmd CursorMoved,InsertLeave,BufEnter,BufWinEnter,TabEnter,BufWritePost *
-\ lua require'lsp_extensions'.inlay_hints{ prefix = '', highlight = "Comment" }
-
--- lua/tools.lua
+++ lua/tools.lua
@@ -26,40 +26,101 @@ function setupLsp()
       }
     )
 
+    -- Code navigation shortcuts
+    vim.cmd[[nnoremap <silent> <c-]> <cmd>lua vim.lsp.buf.definition()<CR>]]
+    vim.cmd[[nnoremap <silent> K     <cmd>lua vim.lsp.buf.hover()<CR>]]
+    vim.cmd[[nnoremap <silent> gD    <cmd>lua vim.lsp.buf.implementation()<CR>]]
+    vim.cmd[[nnoremap <silent> <c-k> <cmd>lua vim.lsp.buf.signature_help()<CR>]]
+    vim.cmd[[nnoremap <silent> 1gD   <cmd>lua vim.lsp.buf.type_definition()<CR>]]
+    vim.cmd[[nnoremap <silent> gr    <cmd>lua vim.lsp.buf.references()<CR>]]
+    vim.cmd[[nnoremap <silent> gR    <cmd>lua vim.lsp.buf.rename()<CR>]]
+    vim.cmd[[nnoremap <silent> g0    <cmd>lua vim.lsp.buf.document_symbol()<CR>]]
+    vim.cmd[[nnoremap <silent> gW    <cmd>lua vim.lsp.buf.workspace_symbol()<CR>]]
+    vim.cmd[[nnoremap <silent> gd    <cmd>lua vim.lsp.buf.declaration()<CR>]]
+    vim.cmd[[nnoremap <silent> ga    <cmd>lua vim.lsp.buf.code_action()<CR>]]
+
+    vim.cmd[[set updatetime=800]]
+
+    -- Show diagnostic popup on cursor hold
+    vim.cmd[[autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics()]]
+
+    -- Goto previous/next diagnostic warning/error
+    vim.cmd[[nnoremap <silent> g[ <cmd>lua vim.lsp.diagnostic.goto_prev()<CR>]]
+    vim.cmd[[nnoremap <silent> g] <cmd>lua vim.lsp.diagnostic.goto_next()<CR>]]
+
+    -- Enable type inlay hints
+    vim.cmd[[
+    autocmd CursorMoved,InsertLeave,BufEnter,BufWinEnter,TabEnter,BufWritePost *
+    \ lua require'lsp_extensions'.inlay_hints{ prefix = '', highlight = "Comment" }]]
+
+    vim.cmd[[set shortmess-=F]]
+    vim.cmd[[set shortmess+=c]]
+
+    vim.cmd[[set completeopt=menuone,noinsert,noselect]]
+
+end
+
+function initAutoformat()
+    vim.g.formatter_path = {"c:/tools"}
+    vim.g.autoformat_autoindent = 0
+    vim.g.autoformat_retab = 0
+    vim.g.formatters_js = {'prettier'}
+    vim.g.formatters_jsx = {'prettier'}
+    vim.g.formatters_ts = {'prettier'}
+    vim.g.formatters_javascript = {'prettier'}
+    vim.g.formatters_typescript = {'prettier'}
+    vim.g.formatters_javascriptreact = {'prettier'}
+    vim.g.formatters_typescriptreact = {'prettier'}
+    vim.g.formatters_html = {'prettier'}
+    vim.g.formatters_css = {'prettier'}
+    vim.g.formatters_json = {'prettier'}
+    vim.g.formatters_toml = {'prettier'}
+    vim.g.formatters_md = {'prettier'}
+    vim.g.formatters_yaml = {'prettier'}
+    vim.g.formatters_svelte = {'prettier'}
+    vim.g.formatters_sql = {}
+    vim.g.formatters_python = {'black'}
+end
+
+function initTokyonight()
+    vim.g.tokyonight_style="night"
+    vim.g.tokyonight_sidebars = { "quickfix", "__vista__", "terminal" }
 end
 
 function M.initialize()
-    api.nvim_command('set background=dark')
-    api.nvim_command('colorscheme frenetiq')
-    api.nvim_command('syntax on')
+    vim.cmd[[set background=dark]]
+    vim.cmd[[colorscheme tokyonight]]
+    vim.cmd[[syntax on]]
 
-    api.nvim_command('set nocompatible')
-    api.nvim_command('filetype plugin indent on')
+    vim.cmd[[set nocompatible]]
+    vim.cmd[[filetype plugin indent on]]
     -- show existing tab with 4 spaces width
-    api.nvim_command('set tabstop=4')
+    vim.cmd[[set tabstop=4]]
     -- when indenting with '>', use 4 spaces width
-    api.nvim_command('set shiftwidth=4')
+    vim.cmd[[set shiftwidth=4]]
     -- On pressing tab, insert 4 spaces
-    api.nvim_command('set expandtab')
-    api.nvim_command('set autoread')
-    api.nvim_command('set number')
-    api.nvim_command('set relativenumber')
-    api.nvim_command('set ignorecase')
-    api.nvim_command('set smartcase')
+    vim.cmd[[set expandtab]]
+    vim.cmd[[set autoread]]
+    vim.cmd[[set number]]
+    vim.cmd[[set relativenumber]]
+    vim.cmd[[set ignorecase]]
+    vim.cmd[[set smartcase]]
 
-    api.nvim_command('vnoremap // y/<C-R>"<CR>')
-    api.nvim_command('noremap <Space> :noh<CR>')
-    api.nvim_command('noremap <A-o> :only<CR>')
-    api.nvim_command('noremap <A-n> :Fern . -drawer -toggle<CR>')
-    api.nvim_command('noremap <A-f> :FernFindCurrentFile<CR>')
-    api.nvim_command('noremap <leader>a :Autoformat')
-    api.nvim_command('noremap <leader>r :LSClient')
+    vim.cmd[[vnoremap // y/<C-R>"<CR>]]
+    vim.cmd[[noremap <Space> :noh<CR>]]
+    vim.cmd[[noremap <A-o> :only<CR>]]
+    vim.cmd[[noremap <A-n> :Fern . -drawer -toggle<CR>]]
+    vim.cmd[[noremap <A-f> :FernFindCurrentFile<CR>]]
+    vim.cmd[[noremap <leader>a :Autoformat]]
+    vim.cmd[[noremap <leader>r :LSClient]]
 
     -- show the next match in the middle of the screen
-    api.nvim_command('noremap n nzz')
-    api.nvim_command('noremap N Nzz')
+    vim.cmd[[noremap n nzz]]
+    vim.cmd[[noremap N Nzz]]
 
     setupLsp()
+    initAutoformat()
+    initTokyonight()
 end
 
 return M
