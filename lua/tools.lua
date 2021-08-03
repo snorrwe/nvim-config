1bdef60a7faf764f88f0695b7cb96fc1cb6ede18
Daniel Kiss <littlesnorrboy@gmail.com>
2021-08-03 18:54:47 +0200

    Use telescope for lsp actions

--- lua/tools.lua
+++ lua/tools.lua
@@ -48,7 +48,7 @@ function setupLsp()
     vim.cmd[[nnoremap <C-k> <cmd>lua require('lspsaga.signaturehelp').signature_help()<cr>]]
     vim.cmd[[nnoremap gR <cmd>lua require('lspsaga.rename').rename()<cr>]]
     vim.cmd[[nnoremap gd <cmd>lua require'lspsaga.provider'.preview_definition()<cr>]]
-    vim.cmd[[nnoremap ga <cmd>lua require('lspsaga.codeaction').code_action()<cr>]]
+    vim.cmd[[nnoremap ga <cmd>Telescope lsp_code_actions<cr>]]
 
     vim.cmd[[set updatetime=300]]
 
@@ -56,9 +56,7 @@ function setupLsp()
     vim.cmd[[autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics()]]
 
     -- Enable type inlay hints
-    vim.cmd[[
-    autocmd CursorMoved,InsertLeave,BufEnter,BufWinEnter,TabEnter,BufWritePost *
-    \ lua require'lsp_extensions'.inlay_hints{ prefix = '', highlight = "Comment" }]]
+    vim.cmd[[autocmd CursorMoved,InsertLeave,BufEnter,BufWinEnter,TabEnter,BufWritePost * lua require'lsp_extensions'.inlay_hints{ prefix = '', highlight = "Comment" }]]
 
     vim.cmd[[set shortmess-=F]]
     vim.cmd[[set shortmess+=c]]
