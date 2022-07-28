97786e53282d998465eece5ef819a5220acf25ad
Daniel Kiss <littlesnorrboy@gmail.com>
2022-07-28 13:41:56 +0200

    Add vim-slash

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -161,10 +161,13 @@ return require('packer').startup(function()
             }
       end
     }
-    use{
+    use {
       "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
       config = function()
         require("lsp_lines").setup()
       end,
+    }
+    use {
+        "junegunn/vim-slash",
     }
 end )
