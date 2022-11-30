c7c63aed0fa59bdba6d80386e8abe7d0888aac87
Daniel Kiss <littlesnorrboy@gmail.com>
2022-11-30 17:09:38 +0100

    Remove dap

--- lua/plugin_setup.lua
+++ lua/plugin_setup.lua
@@ -1,11 +1,11 @@
 local M = {}
 
 function M.setupLsp()
-    require("mason").setup{}
-    require("mason-lspconfig").setup{}
+    require("mason").setup {}
+    require("mason-lspconfig").setup {}
     vim.g.coq_settings = {
-        auto_start=true
-        , ['display.pum.source_context'] = {'[', ']'}
+        auto_start = true
+        , ['display.pum.source_context'] = { '[', ']' }
     }
 
     local nvim_lsp = require 'lspconfig'
@@ -13,7 +13,7 @@ function M.setupLsp()
 
     local servers = { 'rust_analyzer', 'gopls', 'zls', 'pyright', 'tsserver', 'svelte' }
     for _, lsp in ipairs(servers) do
-        local status, retval = pcall( nvim_lsp[lsp].setup, coq.lsp_ensure_capabilities() )
+        local status, retval = pcall(nvim_lsp[lsp].setup, coq.lsp_ensure_capabilities())
         if not status then
             print("lsp setup failed: ", lsp, retval)
         end
@@ -27,33 +27,33 @@ function M.setupLsp()
             capabilities = coq.lsp_ensure_capabilities()
         },
         -- use the defaults
-        extensions = { }
+        extensions = {}
     }
 
     -- Enable diagnostics
     vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-      vim.lsp.diagnostic.on_publish_diagnostics, {
+        vim.lsp.diagnostic.on_publish_diagnostics, {
         update_in_insert = true,
-      }
+    }
     )
 
-    vim.cmd[[set shortmess-=F]]
-    vim.cmd[[set shortmess+=c]]
+    vim.cmd [[set shortmess-=F]]
+    vim.cmd [[set shortmess+=c]]
 end
 
-function M.setupLspSaga() 
+function M.setupLspSaga()
     local saga = require 'lspsaga'
     saga.init_lsp_saga()
 
     -- Code navigation shortcuts
-    vim.cmd[[nnoremap <c-]> <cmd>lua require'lspsaga.provider'.lsp_finder()<cr>]]
-    vim.cmd[[nnoremap go <cmd>Lspsaga show_line_diagnostics<cr>]]
-    vim.cmd[[nnoremap g[ <cmd>Lspsaga diagnostic_jump_prev<cr>]]
-    vim.cmd[[nnoremap g] <cmd>Lspsaga diagnostic_jump_next<cr>]]
-    vim.cmd[[nnoremap K <cmd>Lspsaga hover_doc<cr>]]
-    vim.cmd[[nnoremap gD <cmd>lua vim.lsp.buf.implementation()<cr>]]
-    vim.cmd[[nnoremap gR <cmd>lua require('lspsaga.rename').rename()<cr>]]
-    vim.cmd[[nnoremap gd <cmd>lua require'lspsaga.provider'.preview_definition()<cr>]]
+    vim.cmd [[nnoremap <c-]> <cmd>lua require'lspsaga.provider'.lsp_finder()<cr>]]
+    vim.cmd [[nnoremap go <cmd>Lspsaga show_line_diagnostics<cr>]]
+    vim.cmd [[nnoremap g[ <cmd>Lspsaga diagnostic_jump_prev<cr>]]
+    vim.cmd [[nnoremap g] <cmd>Lspsaga diagnostic_jump_next<cr>]]
+    vim.cmd [[nnoremap K <cmd>Lspsaga hover_doc<cr>]]
+    vim.cmd [[nnoremap gD <cmd>lua vim.lsp.buf.implementation()<cr>]]
+    vim.cmd [[nnoremap gR <cmd>lua require('lspsaga.rename').rename()<cr>]]
+    vim.cmd [[nnoremap gd <cmd>lua require'lspsaga.provider'.preview_definition()<cr>]]
 end
 
 function M.setupAutoformat()
@@ -70,21 +70,21 @@ function M.setupAutoformat()
         }
     }
 
-    vim.cmd[[nnoremap <leader>a <cmd>lua vim.lsp.buf.format()<CR>]]
-    vim.cmd[[vnoremap <leader>a <cmd>lua vim.lsp.buf.format()<CR>]]
+    vim.cmd [[nnoremap <leader>a <cmd>lua vim.lsp.buf.format()<CR>]]
+    vim.cmd [[vnoremap <leader>a <cmd>lua vim.lsp.buf.format()<CR>]]
 end
 
 function M.setupTelescope()
-    require('telescope').setup{
-        defaults ={
-            vimgrep_arguments= {
-              'rg',
-              '--color=never',
-              '--no-heading',
-              '--with-filename',
-              '--line-number',
-              '--column',
-              '--smart-case'
+    require('telescope').setup {
+        defaults = {
+            vimgrep_arguments = {
+                'rg',
+                '--color=never',
+                '--no-heading',
+                '--with-filename',
+                '--line-number',
+                '--column',
+                '--smart-case'
             }
             , file_ignore_patterns = {}
             , mappings = {
@@ -96,50 +96,23 @@ function M.setupTelescope()
             }
         }
     }
-    vim.cmd[[nnoremap <space>f <cmd>lua require('telescope.builtin').find_files()<cr>]]
-    vim.cmd[[nnoremap <space>g <cmd>lua require('telescope.builtin').live_grep()<cr>]]
-    vim.cmd[[nnoremap <space>b <cmd>lua require('telescope.builtin').buffers()<cr>]]
-    vim.cmd[[nnoremap <space>t <cmd>lua require('telescope.builtin').help_tags()<cr>]]
-    vim.cmd[[nnoremap <space>l <cmd>lua require('telescope.builtin').diagnostics()<cr>]]
-    vim.cmd[[nnoremap <space>s <cmd>lua require('telescope.builtin').lsp_document_symbols()<cr>]]
-    vim.cmd[[nnoremap <space>S <cmd>lua require('telescope.builtin').lsp_workspace_symbols()<cr>]]
-    vim.cmd[[nnoremap gr <cmd>lua require('telescope.builtin').lsp_references()<cr>]]
-    vim.cmd[[nnoremap ga <cmd>lua vim.lsp.buf.code_action()<cr>]]
-end
-
-function M.setupDap()
-    local dap = require('dap')
-    dap.configurations.cpp = {
---      {
---        type = 'lldb',
---        request = 'launch',
---        name = "Launch Air Sample",
---      program = "",
---      runInTerminal = true,
---      },
-    }
-    dap.adapters.lldb = {
-      type = 'executable',
-      command = 'lldb-vscode.exe',
-      name = "lldb"
-    }
-
-    vim.cmd[[nnoremap <silent> <F10> :lua require'dap'.step_over()<CR>]]
-    vim.cmd[[nnoremap <silent> <F11> :lua require'dap'.step_into()<CR>]]
-    vim.cmd[[nnoremap <silent> <F12> :lua require'dap'.step_out()<CR>]]
-    vim.cmd[[nnoremap <silent> <leader>dr :lua require'dap'.continue()<CR>]]
-    vim.cmd[[nnoremap <silent> <leader>db :lua require'dap'.toggle_breakpoint()<CR>]]
-    vim.cmd[[nnoremap <silent> <leader>dB :lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>]]
-    vim.cmd[[nnoremap <silent> <leader>dp :lua require'dap'.repl.open()<CR>]]
-    vim.cmd[[nnoremap <silent> <leader>dl :lua require'dap'.run_last()<CR>]]
+    vim.cmd [[nnoremap <space>f <cmd>lua require('telescope.builtin').find_files()<cr>]]
+    vim.cmd [[nnoremap <space>g <cmd>lua require('telescope.builtin').live_grep()<cr>]]
+    vim.cmd [[nnoremap <space>b <cmd>lua require('telescope.builtin').buffers()<cr>]]
+    vim.cmd [[nnoremap <space>t <cmd>lua require('telescope.builtin').help_tags()<cr>]]
+    vim.cmd [[nnoremap <space>l <cmd>lua require('telescope.builtin').diagnostics()<cr>]]
+    vim.cmd [[nnoremap <space>s <cmd>lua require('telescope.builtin').lsp_document_symbols()<cr>]]
+    vim.cmd [[nnoremap <space>S <cmd>lua require('telescope.builtin').lsp_workspace_symbols()<cr>]]
+    vim.cmd [[nnoremap gr <cmd>lua require('telescope.builtin').lsp_references()<cr>]]
+    vim.cmd [[nnoremap ga <cmd>lua vim.lsp.buf.code_action()<cr>]]
 end
 
 function M.setupTS()
-    require'nvim-treesitter.configs'.setup {
+    require 'nvim-treesitter.configs'.setup {
         ensure_installed = {},
         highlight = {
-            enable=true,
-            additional_vim_regex_highlighting=false,
+            enable = true,
+            additional_vim_regex_highlighting = false,
         },
         autopairs = {
             enable = true
@@ -157,71 +130,71 @@ function M.setupTS()
 end
 
 function M.setupTree()
-    require'nvim-tree'.setup {
-          update_cwd          = true,
+    require 'nvim-tree'.setup {
+        update_cwd = true,
     }
 
-    vim.cmd[[nnoremap <silent> <leader>n <cmd>NvimTreeToggle<CR>]]
-    vim.cmd[[nnoremap <silent> <leader>f <cmd>NvimTreeFindFile<CR>]]
+    vim.cmd [[nnoremap <silent> <leader>n <cmd>NvimTreeToggle<CR>]]
+    vim.cmd [[nnoremap <silent> <leader>f <cmd>NvimTreeFindFile<CR>]]
 end
 
 function M.setupBufferline()
-    require("bufferline").setup{}
+    require("bufferline").setup {}
 
-    vim.cmd[[nnoremap <silent> <leader>wd <cmd>BufferLineCycleNext<cr>]]
-    vim.cmd[[nnoremap <silent> <leader>wa <cmd>BufferLineCyclePrev<cr>]]
+    vim.cmd [[nnoremap <silent> <leader>wd <cmd>BufferLineCycleNext<cr>]]
+    vim.cmd [[nnoremap <silent> <leader>wa <cmd>BufferLineCyclePrev<cr>]]
 end
 
 function M.setupFloaterm()
-    vim.cmd[[nnoremap <silent> <leader>tn <cmd>FloatermNew<cr>]]
-    vim.cmd[[nnoremap <silent> <leader>tt <cmd>FloatermToggle<cr>]]
-    vim.cmd[[command! Vifm FloatermNew vifm]]
-    vim.cmd[[command! LazyGit FloatermNew lazygit]]
-    vim.cmd[[nnoremap <silent> <F12> <cmd>FloatermToggle<CR>]]
-    vim.cmd[[tnoremap <silent> <F12> <C-\><C-n><cmd>FloatermToggle<CR>]]
+    vim.cmd [[nnoremap <silent> <leader>tn <cmd>FloatermNew<cr>]]
+    vim.cmd [[nnoremap <silent> <leader>tt <cmd>FloatermToggle<cr>]]
+    vim.cmd [[command! Vifm FloatermNew vifm]]
+    vim.cmd [[command! LazyGit FloatermNew lazygit]]
+    vim.cmd [[nnoremap <silent> <F12> <cmd>FloatermToggle<CR>]]
+    vim.cmd [[tnoremap <silent> <F12> <C-\><C-n><cmd>FloatermToggle<CR>]]
 
-    vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>LazyGit<cr>", {silent=true, noremap=true})
+    vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>LazyGit<cr>", { silent = true, noremap = true })
 
     vim.g.floaterm_width = 0.8
     vim.g.floaterm_height = 0.8
-    vim.g.floaterm_keymap_toggle="<F12>"
+    vim.g.floaterm_keymap_toggle = "<F12>"
 end
 
 function M.initialize()
     vim.o.completeopt = "menuone,noselect"
     -- gui
-    vim.cmd[[set guifont=CaskaydiaCove\ NF:h12]]
+    vim.cmd [[set guifont=CaskaydiaCove\ NF:h12]]
     -- neovide
     vim.g.neovide_cursor_vfx_mode = "railgun"
 
     -- vim.cmd[[set background=dark]]
-    vim.cmd[[syntax on]]
+    vim.cmd [[syntax on]]
 
-    vim.cmd[[set nocompatible]]
-    vim.cmd[[filetype plugin indent on]]
+    vim.cmd [[set nocompatible]]
+    vim.cmd [[filetype plugin indent on]]
     -- show existing tab with 4 spaces width
-    vim.cmd[[set tabstop=4]]
+    vim.cmd [[set tabstop=4]]
     -- when indenting with '>', use 4 spaces width
-    vim.cmd[[set shiftwidth=4]]
+    vim.cmd [[set shiftwidth=4]]
     -- On pressing tab, insert 4 spaces
-    vim.cmd[[set expandtab]]
-    vim.cmd[[set autoread]]
-    vim.cmd[[set number]]
-    vim.cmd[[set relativenumber]]
-    vim.cmd[[set ignorecase]]
-    vim.cmd[[set smartcase]]
-    vim.cmd[[set list]]
+    vim.cmd [[set expandtab]]
+    vim.cmd [[set autoread]]
+    vim.cmd [[set number]]
+    vim.cmd [[set relativenumber]]
+    vim.cmd [[set ignorecase]]
+    vim.cmd [[set smartcase]]
+    vim.cmd [[set list]]
 
-    vim.cmd[[vnoremap // y/<C-R>"<cr>]]
-    vim.cmd[[noremap <leader>o <cmd>only<cr>]]
+    vim.cmd [[vnoremap // y/<C-R>"<cr>]]
+    vim.cmd [[noremap <leader>o <cmd>only<cr>]]
 
     -- show the next match in the middle of the screen
-    vim.cmd[[noremap n nzz]]
-    vim.cmd[[noremap N Nzz]]
-    vim.cmd[[noremap <leader>] :BufferLineCycleNext<cr>]]
-    vim.cmd[[noremap <leader>[ :BufferLineCyclePrev<cr>]]
+    vim.cmd [[noremap n nzz]]
+    vim.cmd [[noremap N Nzz]]
+    vim.cmd [[noremap <leader>] :BufferLineCycleNext<cr>]]
+    vim.cmd [[noremap <leader>[ :BufferLineCyclePrev<cr>]]
 
-    vim.cmd[[autocmd User LspProgressUpdate redrawstatus]]
+    vim.cmd [[autocmd User LspProgressUpdate redrawstatus]]
 end
 
 return M
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,7 +5,7 @@ return require('packer').startup(function()
     use {
         'kyazdani42/nvim-tree.lua',
         requires = {
-          'kyazdani42/nvim-web-devicons', -- optional, for file icon
+            'kyazdani42/nvim-web-devicons', -- optional, for file icon
         },
         config = function()
             require("plugin_setup").setupTree()
@@ -13,15 +13,15 @@ return require('packer').startup(function()
     }
     -- autoformat
     use {
-      "jose-elias-alvarez/null-ls.nvim",
-      event = "BufRead",
-      config = function()
-        require "plugin_setup".setupAutoformat()
-      end,
+        "jose-elias-alvarez/null-ls.nvim",
+        event = "BufRead",
+        config = function()
+            require "plugin_setup".setupAutoformat()
+        end,
     }
     use {
         'neovim/nvim-lspconfig',
-        requires={ "https://github.com/p00f/clangd_extensions.nvim", },
+        requires = { "https://github.com/p00f/clangd_extensions.nvim", },
     }
     use {
         'tami5/lspsaga.nvim',
@@ -31,52 +31,46 @@ return require('packer').startup(function()
     }
     use { 'kyazdani42/nvim-web-devicons' }
     use {
-      'nvim-lualine/lualine.nvim',
-      requires = {'kyazdani42/nvim-web-devicons', opt = true},
-      config = function()
-          require("config_lualine")
-      end
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+        config = function()
+            require("config_lualine")
+        end
     }
     use { 'editorconfig/editorconfig-vim' }
     use {
         'nvim-telescope/telescope.nvim',
-        requires = {{ 'nvim-lua/plenary.nvim' }},
-        config=function()
+        requires = { { 'nvim-lua/plenary.nvim' } },
+        config = function()
             require("plugin_setup").setupTelescope()
         end
     }
     use {
         'ms-jpq/coq_nvim',
         branch = 'coq',
-        requires = { 
+        requires = {
             {
                 'ms-jpq/coq.artifacts',
-                branch='artifacts'
+                branch = 'artifacts'
             },
             {
                 'ms-jpq/coq.thirdparty',
-                branch='3p'
+                branch = '3p'
             }
         },
         config = function()
             require("plugin_setup").setupLsp()
-            vim.cmd[[COQnow]]
+            vim.cmd [[COQnow]]
         end,
         run = function()
-            vim.cmd[[COQdeps]]
+            vim.cmd [[COQdeps]]
         end
 
     }
     use {
-        'mfussenegger/nvim-dap',
-        config = function()
-            require("plugin_setup").setupDap()
-        end
-    }
-    use {
         'akinsho/bufferline.nvim',
         branch = 'main',
-        config=function()
+        config = function()
             require("plugin_setup").setupBufferline()
         end
     }
@@ -85,44 +79,44 @@ return require('packer').startup(function()
         run = ":TSUpdate",
         event = "BufRead",
         cmd = {
-          "TSInstall",
-          "TSInstallInfo",
-          "TSInstallSync",
-          "TSUninstall",
-          "TSUpdate",
-          "TSUpdateSync",
-          "TSDisableAll",
-          "TSEnableAll",
+            "TSInstall",
+            "TSInstallInfo",
+            "TSInstallSync",
+            "TSUninstall",
+            "TSUpdate",
+            "TSUpdateSync",
+            "TSDisableAll",
+            "TSEnableAll",
         },
-        config=function()
+        config = function()
             require("plugin_setup").setupTS()
         end,
         requires = {
-          {
-            -- Parenthesis highlighting
-            "p00f/nvim-ts-rainbow",
-            after = "nvim-treesitter",
-          },
-          {
-            -- Autoclose tags
-            "windwp/nvim-ts-autotag",
-            after = "nvim-treesitter",
-          },
-          {
-            -- Context based commenting
-            "JoosepAlviste/nvim-ts-context-commentstring",
-            after = "nvim-treesitter",
-          },
-          {
-            -- show context
-            "nvim-treesitter/nvim-treesitter-context",
-            after = "nvim-treesitter",
-            config = function()
-                require'treesitter-context'.setup {
-                    enable = true
-                }
-            end,
-          },
+            {
+                -- Parenthesis highlighting
+                "p00f/nvim-ts-rainbow",
+                after = "nvim-treesitter",
+            },
+            {
+                -- Autoclose tags
+                "windwp/nvim-ts-autotag",
+                after = "nvim-treesitter",
+            },
+            {
+                -- Context based commenting
+                "JoosepAlviste/nvim-ts-context-commentstring",
+                after = "nvim-treesitter",
+            },
+            {
+                -- show context
+                "nvim-treesitter/nvim-treesitter-context",
+                after = "nvim-treesitter",
+                config = function()
+                    require 'treesitter-context'.setup {
+                        enable = true
+                    }
+                end,
+            },
         },
     }
     use {
@@ -140,7 +134,7 @@ return require('packer').startup(function()
                     dim_inactive = true,
                 },
             })
-            vim.cmd[[colorscheme duskfox]]
+            vim.cmd [[colorscheme duskfox]]
         end,
         run = function()
             require('nightfox').setup({
@@ -157,15 +151,15 @@ return require('packer').startup(function()
         "folke/which-key.nvim",
         config = function()
             require("which-key").setup {
-                  triggers = "auto",
+                triggers = "auto",
             }
-      end
+        end
     }
     use {
-      "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-      config = function()
-        require("lsp_lines").setup()
-      end,
+        "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+        config = function()
+            require("lsp_lines").setup()
+        end,
     }
     use {
         "junegunn/vim-slash",
@@ -177,10 +171,10 @@ return require('packer').startup(function()
     -- Be sure to configure the installed lsp servers
     -- in plugin_setup.lua / setupLsp
     use {
-        "williamboman/mason.nvim" ,
+        "williamboman/mason.nvim",
         requires = {
             "williamboman/mason-lspconfig.nvim",
             "neovim/nvim-lspconfig",
         },
     }
-end )
+end)
