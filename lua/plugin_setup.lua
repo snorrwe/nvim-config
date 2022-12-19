2b7ca8d49c9c6176990318e7ad6268383c5460d8
Daniel Kiss <littlesnorrboy@gmail.com>
2022-12-19 22:07:30 +0000

    treesitter auto install

--- lua/plugin_setup.lua
+++ lua/plugin_setup.lua
@@ -109,7 +109,9 @@ end
 
 function M.setupTS()
     require 'nvim-treesitter.configs'.setup {
-        ensure_installed = {},
+        ensure_installed = { "lua", "rust", "c", "cpp", "javascript", "typescript", "html", "css" },
+        sync_install = false,
+        auto_install = true,
         highlight = {
             enable = true,
             additional_vim_regex_highlighting = false,
