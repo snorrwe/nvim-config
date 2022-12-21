80dbf532c2a0c663dd2a775d191eb655dca50b1a
Daniel Kiss <littlesnorrboy@gmail.com>
2022-12-21 17:21:22 +0100

    Fix buffers

--- lua/plugin_setup.lua
+++ lua/plugin_setup.lua
@@ -42,7 +42,7 @@ function M.setupTelescope()
     }
     vim.keymap.set('n', '<space>f', "<cmd>lua require('telescope.builtin').find_files()<cr>")
     vim.keymap.set('n', '<space>g', "<cmd>lua require('telescope.builtin').live_grep()<cr>")
-    vim.keymap.set('n', '<space>b <cmd>lua', "require('telescope.builtin').buffers()<cr>")
+    vim.keymap.set('n', '<space>b', "<cmd>lua require('telescope.builtin').buffers()<cr>")
     vim.keymap.set('n', '<space>t', "<cmd>lua require('telescope.builtin').help_tags()<cr>")
     vim.keymap.set('n', '<space>l', "<cmd>lua require('telescope.builtin').diagnostics()<cr>")
     vim.keymap.set('n', '<space>s', "<cmd>lua require('telescope.builtin').lsp_document_symbols()<cr>")
