cf56e9e67d6b2e91b5e9638aac69431e123299d6
Daniel Kiss <littlesnorrboy@gmail.com>
2023-03-12 19:19:10 +0100

    Setup dap-virtual-text

--- lua/setup_debugging.lua
+++ lua/setup_debugging.lua
@@ -2,6 +2,7 @@ return function()
     local dap, dapui = require("dap"), require("dapui")
 
     dapui.setup()
+    require("nvim-dap-virtual-text").setup()
     dap.listeners.after.event_initialized["dapui_config"] = function()
         dapui.open()
     end
