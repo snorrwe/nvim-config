ad55003465ad0f63b9e8a04fb7d327e122bb853f
Daniel Kiss <littlesnorrboy@gmail.com>
2023-03-24 21:25:45 +0100

    Set selection_order='near_cursor'
    This seems to fix the issue with cmp, where the preview window would be
    drawn on top of the completions...

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -156,11 +156,14 @@ return {
 
             local cmp = require('cmp')
             local cmp_mappings = lsp.defaults.cmp_mappings {
-                    ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-Space>'] = cmp.mapping.complete(),
             }
             lsp.setup_nvim_cmp {
                 mapping = cmp_mappings,
-                preselect = cmp.PreselectMode.None
+                preselect = cmp.PreselectMode.None,
+                view = {
+                    entries = { name = 'custom', selection_order = 'near_cursor' }
+                },
             }
             lsp.setup()
             local clangd_lsp = lsp.build_options('clangd', {})
