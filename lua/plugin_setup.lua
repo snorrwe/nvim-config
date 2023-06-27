3cd4532e9018feff90dc80f64a5ba8f5c78fff4c
Daniel Kiss <littlesnorrboy@gmail.com>
2023-06-27 10:21:21 +0200

    Remove "help" from TS ensure installed list

--- lua/plugin_setup.lua
+++ lua/plugin_setup.lua
@@ -20,7 +20,7 @@ end
 
 function M.setupTS()
     require 'nvim-treesitter.configs'.setup {
-        ensure_installed = { "help", "lua", "rust", "c", "cpp", "javascript", "typescript", "html", "css" },
+        ensure_installed = { "lua", "rust", "c", "cpp", "javascript", "typescript", "html", "css" },
         sync_install = false,
         auto_install = true,
         highlight = {
