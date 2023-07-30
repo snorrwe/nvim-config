ebf8d4647fda7162ed0c7228f5a48c3e57d92694
Daniel Kiss <littlesnorrboy@gmail.com>
2023-07-30 15:36:05 +0200

    Add current buffer fuzzy find telescope keymap

--- lua/setup_telescope.lua
+++ lua/setup_telescope.lua
@@ -24,6 +24,7 @@ return function()
     }
     vim.keymap.set('n', '<space>f', "<cmd>lua require('telescope.builtin').find_files()<cr>")
     vim.keymap.set('n', '<space>g', "<cmd>lua require('telescope.builtin').live_grep()<cr>")
+    vim.keymap.set('n', '<space>G', "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>")
     vim.keymap.set('n', '<space>b', "<cmd>lua require('telescope.builtin').buffers()<cr>")
     vim.keymap.set('n', '<space>t', "<cmd>lua require('telescope.builtin').help_tags()<cr>")
     vim.keymap.set('n', '<space>l', "<cmd>lua require('telescope.builtin').diagnostics()<cr>")
