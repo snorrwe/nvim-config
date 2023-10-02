4ae23032f84d7c674c4a2e8cfa9e169c51db43be
Daniel Kiss <littlesnorrboy@gmail.com>
2023-10-03 00:14:03 +0200

    Only setup Oil when it's actually used

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -28,6 +28,7 @@ return {
     {
         'stevearc/oil.nvim',
         dependencies = { "nvim-tree/nvim-web-devicons" },
+        cmd = 'Oil',
         config = function()
             require("oil").setup()
         end
