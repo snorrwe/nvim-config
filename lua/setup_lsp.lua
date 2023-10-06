f89cf0f4767dd6502b18a1c39385c005bee1f756
Daniel Kiss <littlesnorrboy@gmail.com>
2023-10-06 09:23:46 +0200

    Update deps
    lsp-zero had breaking api changes

--- lua/setup_lsp.lua
+++ lua/setup_lsp.lua
@@ -1,29 +1,32 @@
 return function()
     local lsp = require('lsp-zero')
+    local mason = require('mason')
+    local mason_lsp = require('mason-lspconfig')
     local clangd_extensions = require('clangd_extensions')
     local rust_rools = require('rust-tools')
+    local cmp = require('cmp')
+    local cmp_action = require('lsp-zero').cmp_action()
+
     lsp.preset('recommended')
-    lsp.ensure_installed({
+    mason.setup{}
+    mason_lsp.setup{
+        ensure_installed={
         'rust_analyzer',
         'clangd',
-    })
+    }}
     lsp.set_preferences({
         suggest_lsp_servers = true,
         set_lsp_keymaps = {
             omit = { '<C-k>', 'gr' },
         }
     })
-    -- clangd is set up by clangd_extensions
-    -- rust_analyzer is set up by rust-tools
-    lsp.skip_server_setup({ 'rust_analyzer' })
 
-    local cmp = require('cmp')
     local cmp_mappings = lsp.defaults.cmp_mappings {
         ['<C-Space>'] = cmp.mapping.complete(),
         ["<CR>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
     }
-    lsp.setup_nvim_cmp {
-        mapping = cmp_mappings,
+    cmp.setup{
+        mapping=cmp_mappings,
         preselect = cmp.PreselectMode.Item,
         formatting = {
             format = function(_, vim_item)
