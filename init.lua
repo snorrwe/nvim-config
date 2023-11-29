1766ad17b0283536bc788b912437cda12de44c72
Daniel Kiss <littlesnorrboy@gmail.com>
2023-11-29 16:21:47 +0100

    fmt

--- init.lua
+++ init.lua
@@ -8,14 +8,14 @@ vim.g.python3_host_prog = HOME .. "/python3/bin/python"
 --
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -33,7 +33,8 @@ vim.cmd([[set tabstop=4]])
 vim.cmd([[set shiftwidth=4]])
 -- On pressing tab, insert 4 spaces
 vim.cmd([[set expandtab]])
-vim.cmd([[set autoread]])
+vim.opt.autoread = true
+
 vim.cmd([[set number]])
 vim.cmd([[set relativenumber]])
 vim.cmd([[set ignorecase]])
@@ -64,33 +65,33 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- wgsl
 local wgsl_group = vim.api.nvim_create_augroup("wgsl", { clear = true })
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-	pattern = { "*.wgsl" },
-	group = wgsl_group,
-	command = "set ft=wgsl",
+    pattern = { "*.wgsl" },
+    group = wgsl_group,
+    command = "set ft=wgsl",
 })
 -- templ
 local templ_group = vim.api.nvim_create_augroup("templ", { clear = true })
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-	pattern = { "*.templ" },
-	group = templ_group,
-	command = "set ft=templ",
+    pattern = { "*.templ" },
+    group = templ_group,
+    command = "set ft=templ",
 })
 
 if vim.fn.has("nvim-0.10") == 1 then
-	-- enable inlay hints on lsp attach
-	vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
-	vim.api.nvim_create_autocmd("LspAttach", {
-		group = "LspAttach_inlayhints",
-		callback = function(args)
-			if not (args.data and args.data.client_id) then
-				return
-			end
+    -- enable inlay hints on lsp attach
+    vim.api.nvim_create_augroup("LspAttach_inlayhints", { clear = true })
+    vim.api.nvim_create_autocmd("LspAttach", {
+        group = "LspAttach_inlayhints",
+        callback = function(args)
+            if not (args.data and args.data.client_id) then
+                return
+            end
 
-			local bufnr = args.buf
-			local client = vim.lsp.get_client_by_id(args.data.client_id)
-			if client.server_capabilities.inlayHintProvider then
-				vim.lsp.inlay_hint(bufnr, true)
-			end
-		end,
-	})
+            local bufnr = args.buf
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+            if client.server_capabilities.inlayHintProvider then
+                vim.lsp.inlay_hint(bufnr, true)
+            end
+        end,
+    })
 end
