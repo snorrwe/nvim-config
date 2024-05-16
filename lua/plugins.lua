842602cc4d0c9608a1d0a75a5ade0ea9d223c033
Daniel Kiss <littlesnorrboy@gmail.com>
2024-05-16 15:07:38 +0200

    Move autocompletion out of lsp deps

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -167,26 +167,26 @@ return {
             },
             { "nvim-telescope/telescope.nvim" },
 
-            -- Autocompletion
+        },
+    },
+    -- Autocompletion
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        config = require("setup_cmp"),
+        dependencies = {
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-path" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            -- Snippets
+            { "saadparwaiz1/cmp_luasnip" },
             {
-                "hrsh7th/nvim-cmp",
-                event = "InsertEnter",
-                config = require("setup_cmp"),
-                dependencies = {
-                    { "hrsh7th/cmp-buffer" },
-                    { "hrsh7th/cmp-path" },
-                    { "hrsh7th/cmp-nvim-lsp" },
-                    -- Snippets
-                    { "saadparwaiz1/cmp_luasnip" },
-                    {
-                        "L3MON4D3/LuaSnip",
-                        version = "v2.*",
-                    },
-                    {
-                        "kristijanhusak/vim-dadbod-completion",
-                    }
-                },
+                "L3MON4D3/LuaSnip",
+                version = "v2.*",
             },
+            {
+                "kristijanhusak/vim-dadbod-completion",
+            }
         },
     },
     {
