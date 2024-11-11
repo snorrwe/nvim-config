1fd84267f64d0d2b230bf485c3f09d101b4863d1
Daniel Kiss <littlesnorrboy@gmail.com>
2024-11-11 13:13:28 +0100

    fmt

--- lua/setup_treesitter.lua
+++ lua/setup_treesitter.lua
@@ -1,50 +1,50 @@
 return function()
-	require("nvim-treesitter.configs").setup({
-		ensure_installed = { "lua", "rust", "c", "cpp", "javascript", "typescript", "html", "css" },
-		sync_install = false,
-		auto_install = true,
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = false,
-		},
-		autopairs = {
-			enable = true,
-		},
-		indent = {
-			enable = true,
-		},
-		rainbow = {
-			enable = true,
-		},
-		autotag = {
-			enable = true,
-		},
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "=",
-				node_incremental = "=",
-				node_decremental = "-",
-				scope_incremental = "+",
-			},
-		},
-		textobjects = {
-			move = {
-				enable = true,
-				set_jumps = true,
-				goto_next_start = {
-					["]m"] = "@function.outer",
-				},
-				goto_previous_start = {
-					["[m"] = "@function.outer",
-				},
-				goto_next_end = {
-					["]M"] = "@function.outer",
-				},
-				goto_previous_end = {
-					["[M"] = "@function.outer",
-				},
-			},
-		},
-	})
+    require("nvim-treesitter.configs").setup({
+        ensure_installed = { "lua", "rust", "c", "cpp", "javascript", "typescript", "html", "css" },
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+        },
+        autopairs = {
+            enable = true,
+        },
+        indent = {
+            enable = true,
+        },
+        rainbow = {
+            enable = true,
+        },
+        autotag = {
+            enable = true,
+        },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "=",
+                node_incremental = "=",
+                node_decremental = "-",
+                scope_incremental = "+",
+            },
+        },
+        textobjects = {
+            move = {
+                enable = true,
+                set_jumps = true,
+                goto_next_start = {
+                    ["]m"] = "@function.outer",
+                },
+                goto_previous_start = {
+                    ["[m"] = "@function.outer",
+                },
+                goto_next_end = {
+                    ["]M"] = "@function.outer",
+                },
+                goto_previous_end = {
+                    ["[M"] = "@function.outer",
+                },
+            },
+        },
+    })
 end
