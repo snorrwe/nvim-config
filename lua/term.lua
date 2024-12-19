a11e7b63d1a50a760970df3ee19bb96c7d7ef9e7
Daniel Kiss <littlesnorrboy@gmail.com>
2024-12-19 09:33:45 +0100

    Add a "just build" keymap

---
+++ lua/term.lua
@@ -0,0 +1,14 @@
+vim.keymap.set("t", "<ESC><ESC>", "<C-\\><C-N>")
+
+local function smallterm()
+    vim.cmd.vnew()
+    vim.cmd.term()
+    vim.cmd.wincmd("J")
+    vim.api.nvim_win_set_height(0, 15)
+    return vim.bo.channel
+end
+
+vim.keymap.set("n", "<leader>jb", function()
+    local job_id = smallterm()
+    vim.fn.chansend(job_id, { "just build\r\n" })
+end)
