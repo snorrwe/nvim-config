3b0f9966e78c1139c302c03584369e2ef2789e32
Daniel Kiss <littlesnorrboy@gmail.com>
2025-01-27 09:53:30 +0100

    Add .cc to clangd custom setup pattern

--- lua/setup_lsp.lua
+++ lua/setup_lsp.lua
@@ -70,7 +70,7 @@ return function()
 
     local group = vim.api.nvim_create_augroup("UserLspConfig", {})
     vim.api.nvim_create_autocmd("LspAttach", {
-        pattern = { "*.c", "*.h", "*.hpp", "*.cpp", "*.cu", "*.cuh" },
+        pattern = { "*.c", "*.h", "*.hpp", "*.cpp", "*.cu", "*.cuh", "*.cc" },
         group = group,
         callback = function(ev)
             local opts = { buffer = ev.buf }
