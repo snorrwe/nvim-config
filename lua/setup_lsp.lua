acddcee836ebc60316949210e021e495e5bc93d8
Daniel Kiss <littlesnorrboy@gmail.com>
2025-03-25 21:16:52 +0100

    Migrate to blink

--- lua/setup_lsp.lua
+++ lua/setup_lsp.lua
@@ -1,6 +1,6 @@
 return function()
     local lsp = require("lspconfig")
-    local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+    local lsp_capabilities = require("blink.cmp").get_lsp_capabilities()
     local mason = require("mason")
     local mason_lsp = require("mason-lspconfig")
     local mason_reg = require("mason-registry")
