dff65362bc871f723e0ecac6c20fd72432b30cc6
Daniel Kiss <littlesnorrboy@gmail.com>
2025-06-13 22:19:08 +0200

    Add Today and Now

--- init.lua
+++ init.lua
@@ -8,14 +8,14 @@ vim.g.python3_host_prog = HOME .. "/python3/bin/python"
 --
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -64,46 +64,49 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- wgsl
 local wgsl_group = vim.api.nvim_create_augroup("wgsl", { clear = true })
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-	pattern = { "*.wgsl" },
-	group = wgsl_group,
-	command = "set ft=wgsl",
+    pattern = { "*.wgsl" },
+    group = wgsl_group,
+    command = "set ft=wgsl",
 })
 -- templ
 local templ_group = vim.api.nvim_create_augroup("templ", { clear = true })
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-	pattern = { "*.templ" },
-	group = templ_group,
-	command = "set ft=templ",
+    pattern = { "*.templ" },
+    group = templ_group,
+    command = "set ft=templ",
 })
 
 if vim.fn.has("nvim-0.10") == 1 then
-	-- enable inlay hints on lsp attach
-	vim.api.nvim_create_augroup("LspAttach_inlayhints", { clear = true })
-	vim.api.nvim_create_autocmd("LspAttach", {
-		group = "LspAttach_inlayhints",
-		callback = function(args)
-			if not (args.data and args.data.client_id) then
-				return
-			end
+    -- enable inlay hints on lsp attach
+    vim.api.nvim_create_augroup("LspAttach_inlayhints", { clear = true })
+    vim.api.nvim_create_autocmd("LspAttach", {
+        group = "LspAttach_inlayhints",
+        callback = function(args)
+            if not (args.data and args.data.client_id) then
+                return
+            end
 
-			local client = vim.lsp.get_client_by_id(args.data.client_id)
-			if client.server_capabilities.inlayHintProvider then
-				vim.lsp.inlay_hint.enable(true)
-			end
-		end,
-	})
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+            if client.server_capabilities.inlayHintProvider then
+                vim.lsp.inlay_hint.enable(true)
+            end
+        end,
+    })
 end
 
 -- highlight when yanking
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight yanked text",
-	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank({ timeout = 200 })
-	end,
+    desc = "Highlight yanked text",
+    group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank({ timeout = 200 })
+    end,
 })
 
 -- Oil is lazy initialized so I put the keymap here instead of config()
 vim.keymap.set("n", "<leader>o", "<cmd>Oil<CR>")
+
+vim.api.nvim_create_user_command("Today", 'r! date +"\\%B \\%d, \\%Y"', {})
+vim.api.nvim_create_user_command("Now", "r! date", {})
 
 require("term")
